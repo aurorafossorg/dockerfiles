4d94c9ca2a6ff33528a01a6e59a8fc642d2977a6
Luís Ferreira <contact@lsferreira.net>
2019-10-19 00:29:39 +0100

    archlinux: make some little improvements
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- archlinux/armv7/bootstrap.sh
+++ archlinux/armv7/bootstrap.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
+#                                     __
 #                                    / _|
 #   __ _ _   _ _ __ ___  _ __ __ _  | |_ ___  ___ ___
 #  / _` | | | | '__/ _ \| '__/ _` | |  _/ _ \/ __/ __|
 # | (_| | |_| | | | (_) | | | (_| | | || (_) \__ \__ \
 #  \__,_|\__,_|_|  \___/|_|  \__,_| |_| \___/|___/___/
 #
-# Copyright (C) 2018 Aurora Free Open Source Software.
+# Copyright (C) 2018-2019 Aurora Free Open Source Software.
+# Copyright (C) 2018-2019 Luís Ferreira <luis@aurorafoss.org>
 #
 # This file is part of the Aurora Free Open Source Software. This
 # organization promote free and open source software that you can
